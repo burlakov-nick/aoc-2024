11ff0780dcaf5935b8ddafa0d464922e67c30e3b
Burlakov Nick <nick@burlakov.io>
2024-12-03 08:16:13 +0100

    join lines

--- day_03/run.py
+++ day_03/run.py
@@ -3,23 +3,21 @@ import re
 
 
 def solve(r: aoc.Reader) -> None:
-    lines = r.read_lines()
+    line = "".join(r.read_lines())
 
     print("Part One")
     res = 0
-    for line in lines:
-        for x, y in re.findall(r"mul\((\d+),(\d+)\)", line):
-            res += int(x) * int(y)
+    for x, y in re.findall(r"mul\((\d+),(\d+)\)", line):
+        res += int(x) * int(y)
     print(res)
 
     print("Part Two")
     res, enabled = 0, True
-    for line in lines:
-        for x, y, do, dont in re.findall(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))", line):
-            if do:
-                enabled = True
-            elif dont:
-                enabled = False
-            elif enabled:
-                res += int(x) * int(y)
+    for x, y, do, dont in re.findall(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))", line):
+        if do:
+            enabled = True
+        elif dont:
+            enabled = False
+        elif enabled:
+            res += int(x) * int(y)
     print(res)
