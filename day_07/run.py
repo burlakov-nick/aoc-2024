f86085062638fe81daa524f1b8ecd663a59db1c5
Burlakov Nick <nick@burlakov.io>
2024-12-07 09:43:35 +0100

    use builtin operators

--- day_07/run.py
+++ day_07/run.py
@@ -1,26 +1,23 @@
 import aoc
 from itertools import product
+from operator import add, mul
 
 
-def can_solve(line, allowed_operations):
+def can_solve(line, allowed_ops):
     expected, *numbers = line
-    for operators in product(allowed_operations, repeat=len(numbers) - 1):
+    for operators in product(allowed_ops, repeat=len(numbers) - 1):
         actual = numbers[0]
         for arg, operator in zip(numbers[1:], operators):
-            if operator == "*":
-                actual *= arg
-            elif operator == "+":
-                actual += arg
-            else:
-                actual = int(str(actual) + str(arg))
+            actual = operator(actual, arg)
         if actual == expected:
-            return True
+            return actual
     return False
 
 
 def solve(r: aoc.Reader) -> None:
     lines = r.read(remove=":")
     print("Part One")
-    print(sum(line[0] for line in lines if can_solve(line, "+*")))
+    print(sum(line[0] for line in lines if can_solve(line, [add, mul])))
     print("Part Two")
-    print(sum(line[0] for line in lines if can_solve(line, "+*|")))
+    concat = lambda x, y: int(str(x) + str(y))
+    print(sum(line[0] for line in lines if can_solve(line, [add, mul, concat])))
