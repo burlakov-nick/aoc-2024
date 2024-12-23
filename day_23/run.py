25635b8d0f29b304cd2021c6cc9a1d1c40557131
Burlakov Nick <nick@burlakov.io>
2024-12-23 10:33:45 +0100

    clean up day 23

--- day_23/run.py
+++ day_23/run.py
@@ -1,25 +1,23 @@
 import networkx
+from networkx.algorithms.clique import enumerate_all_cliques, find_cliques
+
 import aoc
 
 
 def solve(r: aoc.Reader) -> None:
     edges = [tuple(x.split("-")) for x in r.read_lines()]
 
-    G = networkx.Graph()
-    G.add_edges_from(edges)
+    graph = networkx.Graph()
+    graph.add_edges_from(edges)
 
     print("Part One")
-
-    result = 0
-    for clique in networkx.algorithms.clique.enumerate_all_cliques(G):
-        if len(clique) == 3 and any(x.startswith("t") for x in clique):
-            result += 1
-    print(result)
+    print(sum(
+        1 for clique in enumerate_all_cliques(graph)
+        if len(clique) == 3 and any(x.startswith("t") for x in clique)
+    ))
 
     print("Part Two")
-
-    max_clique = max(len(c) for c in networkx.find_cliques(G))
-
-    for clique in networkx.algorithms.clique.enumerate_all_cliques(G):
+    max_clique = max(len(c) for c in find_cliques(graph))
+    for clique in enumerate_all_cliques(graph):
         if len(clique) == max_clique:
             print(",".join(sorted(clique)))
