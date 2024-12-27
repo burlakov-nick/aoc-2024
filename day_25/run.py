809da99fece81298e206797f61a1e8600c87f4b2
Burlakov Nick <nick@burlakov.io>
2024-12-27 15:20:32 +0100

    decrypt

--- day_25/run.py
+++ day_25/run.py
@@ -5,4 +5,7 @@ from helpers import transpose, count
 def solve(r: aoc.Reader) -> None:
     blocks = r.read_blocks(lambda x: x)
     print("Part One")
-    print(sum(1 for lock in [[count(row, "#") - 1 for row in transpose(block)] for block in blocks if block[0] == "#####"] for key in [[count(row, "#") - 1 for row in transpose(block)] for block in blocks if block[0] == "....."] if all(x + y <= 5 for x, y in zip(lock, key))))
+    to_heights = lambda block: [count(row, "#") - 1 for row in transpose(block)]
+    locks = [to_heights(block) for block in blocks if block[0] == "#####"]
+    keys = [to_heights(block) for block in blocks if block[0] == "....."]
+    print(sum(1 for lock in locks for key in keys if all(x + y <= 5 for x, y in zip(lock, key))))
